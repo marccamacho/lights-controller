a00b6d1bb999e793b4a0bc1c1981bf69bfae61d9
Marc Camacho Cateura <camachocateura@gmail.com>
2017-07-17 18:45:08 +0200

    More changes. Preview draft

--- src/app/components/output/output.component.ts
+++ src/app/components/output/output.component.ts
@@ -9,18 +9,32 @@ import { OutputDevice }         from '../../classes/output-device'
 })
 
 export class OutputComponent implements OnInit {
+    currentColor : string;
 
     @Input () currentDevice: OutputDevice = {typeTag:"LED", outputPin:"A13", name:"PIN:", description:"hola què tal?"};
+    @Input () changeColor: boolean = true;
 
-    constructor() { }
+    constructor() {
+        this.currentColor = "rgba(0, 0, 0, 0.54)";
+    }
 
     ngOnInit() {
       console.log(this.currentDevice);
     }
 
-
     ngOnChange() {
       console.log(this.currentDevice);
     }
+
+    toggleColor() {
+        console.log("Change color")
+        if (this.changeColor) {
+            if (this.currentColor == "rgba(0, 0, 0, 0.54)"){
+                this.currentColor = "rgba(43, 189, 126, 0.54)";
+            }
+            else{
+                this.currentColor = "rgba(0, 0, 0, 0.54)";
+            }
+        }
+    }
 }
-  
