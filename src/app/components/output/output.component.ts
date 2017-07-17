8afb49d478cb8898b1cdf1b820631d6363243ead
Alumne <gaguilar28@fedac.cat>
2017-07-17 09:48:45 +0200

    New structure merging

--- src/app/components/output/output.component.ts
+++ src/app/components/output/output.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit }    from '@angular/core';
+import { OutputDevice }         from '../../classes/output-device'
 
 @Component({
-  selector: 'output',
-  templateUrl: './output.component.html',
-  styleUrls: ['./output.component.scss']
+  selector: 'config-output',
+  templateUrl: './config-output.component.html',
+  styleUrls: ['./config-output.component.scss']
 })
+
 export class OutputComponent implements OnInit {
 
-  constructor() { }
+    currentDevice: OutputDevice = {typeTag:"LED", outputPin:"A13", name:"PIN:", description:"hola què tal?"};
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
