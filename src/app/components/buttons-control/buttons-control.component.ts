f61bee8b916b2391385c89a7ca851a24504fa13c
Marc Camacho Cateura <camachocateura@gmail.com>
2017-07-17 12:22:39 +0200

    New component

---
+++ src/app/components/buttons-control/buttons-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'buttons-control',
+  templateUrl: './buttons-control.component.html',
+  styleUrls: ['./buttons-control.component.scss']
+})
+export class ButtonsControlsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
