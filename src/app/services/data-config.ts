7d84a270b0462d11ec5c5ad17f86004bdc0c16e0
sparklester <sparklester1@gmail.com>
2017-07-18 13:00:57 +0200

    Dialog styles improved

--- src/app/services/data-config.ts
+++ src/app/services/data-config.ts
@@ -12,22 +12,22 @@ export class DataConfigService {
             this.dataConfig = {
                         typeTags: ["LED", "Fan", "Light Bulb", "Speaker"],
                         conf: {
-                            out_1:{typeTag:"LED", outputPin:"out_1", name:"Nom", description:"hola què tal?"},
-                            out_2:{typeTag:"LED", outputPin:"out_2", name:"Nom", description:"hola què tal?"},
-                            out_3:{typeTag:"LED", outputPin:"out_3", name:"Nom", description:"hola què tal?"},
-                            out_4:{typeTag:"LED", outputPin:"out_4", name:"Nom", description:"hola què tal?"},
-                            out_5:{typeTag:"LED", outputPin:"out_5", name:"Nom", description:"hola què tal?"},
-                            out_6:{typeTag:"LED", outputPin:"out_6", name:"Nom", description:"hola què tal?"},
-                            out_7:{typeTag:"LED", outputPin:"out_7", name:"Nom", description:"hola què tal?"},
-                            out_8:{typeTag:"LED", outputPin:"out_8", name:"Nom", description:"hola què tal?"},
-                            out_9:{typeTag:"LED", outputPin:"out_9", name:"Nom", description:"hola què tal?"},
-                            out_10:{typeTag:"LED", outputPin:"out_10", name:"Nom", description:"hola què tal?"},
-                            out_11:{typeTag:"LED", outputPin:"out_11", name:"Nom", description:"hola què tal?"},
-                            out_12:{typeTag:"LED", outputPin:"out_12", name:"Nom", description:"hola què tal?"},
-                            out_13:{typeTag:"LED", outputPin:"out_13", name:"Nom", description:"hola què tal?"},
-                            out_14:{typeTag:"LED", outputPin:"out_14", name:"Nom", description:"hola què tal?"},
-                            out_15:{typeTag:"LED", outputPin:"out_15", name:"Nom", description:"hola què tal?"},
-                            out_16:{typeTag:"LED", outputPin:"out_16", name:"Nom", description:"hola què tal?"}
+                            out_1:{typeTag:"LED", outputPin:"out_1", name:"Name", description:"Sample"},
+                            out_2:{typeTag:"LED", outputPin:"out_2", name:"Name", description:"Sample"},
+                            out_3:{typeTag:"LED", outputPin:"out_3", name:"Name", description:"Sample"},
+                            out_4:{typeTag:"LED", outputPin:"out_4", name:"Name", description:"Sample"},
+                            out_5:{typeTag:"LED", outputPin:"out_5", name:"Name", description:"Sample"},
+                            out_6:{typeTag:"LED", outputPin:"out_6", name:"Name", description:"Sample"},
+                            out_7:{typeTag:"LED", outputPin:"out_7", name:"Name", description:"Sample"},
+                            out_8:{typeTag:"LED", outputPin:"out_8", name:"Name", description:"Sample"},
+                            out_9:{typeTag:"LED", outputPin:"out_9", name:"Name", description:"Sample"},
+                            out_10:{typeTag:"LED", outputPin:"out_10", name:"Name", description:"Sample"},
+                            out_11:{typeTag:"LED", outputPin:"out_11", name:"Name", description:"Sample"},
+                            out_12:{typeTag:"LED", outputPin:"out_12", name:"Name", description:"Sample"},
+                            out_13:{typeTag:"LED", outputPin:"out_13", name:"Name", description:"Sample"},
+                            out_14:{typeTag:"LED", outputPin:"out_14", name:"Name", description:"Sample"},
+                            out_15:{typeTag:"LED", outputPin:"out_15", name:"Name", description:"Sample"},
+                            out_16:{}
                         }
             }
         } else {
