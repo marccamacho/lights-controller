6f13effd1e89c6dc835381bd42ec1451d3fcdadc
Marc Camacho Cateura <camachocateura@gmail.com>
2017-07-19 16:40:32 +0200

    New dist

---
+++ src/app/services/data-config.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DataConfigService {
+
+    serverIP    : string = "http://192.168.0.122:5000/";  // Server IP
+    dataConfig  : any;          // Configuration data in JSON format. See README.md.
+    currentPage : string = "TAULA DE CONTROL";
+
+    constructor() {
+        if (this.serverIP != "") {
+            this.dataConfig = {
+                        typeTags: ["LED", "Fan", "Light Bulb", "Speaker"],
+                        conf: {
+                            3:{typeTag:"LED", outputPin:"3", name:"Name", description:"Sample"},
+                            5:{typeTag:"LED", outputPin:"5", name:"Name", description:"Sample"},
+                            7:{typeTag:"LED", outputPin:"7", name:"Name", description:"Sample"},
+                            29:{typeTag:"LED", outputPin:"29", name:"Name", description:"Sample"},
+                            31:{typeTag:"LED", outputPin:"31", name:"Name", description:"Sample"},
+                            33:{typeTag:"LED", outputPin:"33", name:"Name", description:"Sample"},
+                            35:{typeTag:"LED", outputPin:"35", name:"Name", description:"Sample"},
+                            37:{typeTag:"LED", outputPin:"37", name:"Name", description:"Sample"}
+                        }
+            }
+        } else {
+            this.dataConfig = this.getRemoteConfig();
+        }
+    }
+
+    // Get configuration JSON from server
+    getRemoteConfig () {
+
+    }
+
+    // Update configuration JSON from server
+    putRemoteConfig () {
+
+    }
+
+    // Update default configuration JSON from server
+    defaultRemoteConfig () {
+        // Get default configuration from server
+    }
+
+    // Export configuration JSON
+    exportConfig () {
+        // Download file
+    }
+
+    // Import configuration JSON
+    importConfig (data : any) {
+
+    }
+}
