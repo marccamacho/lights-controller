572b32d9dffd876fa204c811414a5b55b1513d0d
Guipillempe, Marcus i Arnau productions <grexach17@fedac.cat>
2017-07-20 10:03:26 +0200

    Added html part of program component

--- src/app/components/program/program.component.ts
+++ src/app/components/program/program.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Program } from '../../classes/program-query';
 
 @Component({
   selector: 'app-program',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./program.component.scss']
 })
 export class ProgramComponent implements OnInit {
-
+  @Input() currentProgram:Program;        
   constructor() { }
 
   ngOnInit() {
